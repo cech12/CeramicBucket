25b93f085e9bb588e254dc8b49916d0da865f996
Christian Voß <cech12@gmail.com>
2020-08-11 00:35:43 +0200

    - fixed: sometimes new model loader was not registered

--- src/main/java/cech12/ceramicbucket/client/ClientEvents.java
+++ src/main/java/cech12/ceramicbucket/client/ClientEvents.java
@@ -4,16 +4,16 @@ import cech12.ceramicbucket.CeramicBucketMod;
 import cech12.ceramicbucket.client.model.CeramicBucketModel;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid= CeramicBucketMod.MOD_ID, bus= Mod.EventBusSubscriber.Bus.MOD, value= Dist.CLIENT)
 public class ClientEvents {
 
     @SubscribeEvent
-    public static void registerModels(ModelRegistryEvent event) {
+    public static void clientSetup(FMLClientSetupEvent event) {
         ModelLoaderRegistry.registerLoader(new ResourceLocation(CeramicBucketMod.MOD_ID, "bucket"), CeramicBucketModel.Loader.INSTANCE);
     }
 }
