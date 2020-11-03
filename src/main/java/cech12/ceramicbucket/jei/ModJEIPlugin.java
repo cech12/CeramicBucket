f9e0e1406276158d871b9c5e51ac4b7528122d23
Christian Voß <cech12@gmail.com>
2020-11-03 15:36:40 +0100

    - add infinity enchantment to jei - disable entity buckets to be enchantable with infinity - filled buckets with infinity cannot be transformed to entity buckets

--- src/main/java/cech12/ceramicbucket/jei/ModJEIPlugin.java
+++ src/main/java/cech12/ceramicbucket/jei/ModJEIPlugin.java
@@ -2,17 +2,34 @@ package cech12.ceramicbucket.jei;
 
 import cech12.ceramicbucket.CeramicBucketMod;
 import cech12.ceramicbucket.api.item.CeramicBucketItems;
+import cech12.ceramicbucket.config.ServerConfig;
+import cech12.ceramicbucket.item.FilledCeramicBucketItem;
+import cech12.ceramicbucket.util.CeramicBucketUtils;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaRecipeCategoryUid;
+import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
+import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @JeiPlugin
 public class ModJEIPlugin implements IModPlugin {
 
-    private static ResourceLocation ID = new ResourceLocation(CeramicBucketMod.MOD_ID, "jei_plugin");
+    private static final ResourceLocation ID = new ResourceLocation(CeramicBucketMod.MOD_ID, "jei_plugin");
 
     @Nonnull
     @Override
@@ -24,6 +41,34 @@ public class ModJEIPlugin implements IModPlugin {
     public void registerItemSubtypes(ISubtypeRegistration registration) {
         registration.useNbtForSubtypes(CeramicBucketItems.FILLED_CERAMIC_BUCKET);
         registration.useNbtForSubtypes(CeramicBucketItems.CERAMIC_ENTITY_BUCKET);
+    }
+
+    @Override
+    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
+        if (ServerConfig.INFINITY_ENCHANTMENT_ENABLED.get()) {
+            IVanillaRecipeFactory factory = registration.getVanillaRecipeFactory();
+            EnchantmentData data = new EnchantmentData(Enchantments.INFINITY, Enchantments.INFINITY.getMaxLevel());
+            List<Object> recipes = new ArrayList<>();
+            List<Fluid> addedFluids = new ArrayList<>();
+            for (Fluid fluid : ForgeRegistries.FLUIDS) {
+                FluidUtil.getFluidContained(new ItemStack(fluid.getFilledBucket())).ifPresent(bucketFluidStack -> {
+                    Fluid bucketFluid = bucketFluidStack.getFluid();
+                    if (bucketFluid instanceof FlowingFluid && !addedFluids.contains(bucketFluid)
+                            && CeramicBucketUtils.canFluidSourcesMultiply((FlowingFluid) bucketFluid)) {
+                        addedFluids.add(bucketFluid);
+                        ItemStack bucket = ((FilledCeramicBucketItem) CeramicBucketItems.FILLED_CERAMIC_BUCKET).getFilledInstance(bucketFluid.getFluid());
+                        ItemStack enchantedBucket = bucket.copy();
+                        enchantedBucket.addEnchantment(data.enchantment, data.enchantmentLevel);
+                        recipes.add(factory.createAnvilRecipe(bucket,
+                                Collections.singletonList(EnchantedBookItem.getEnchantedItemStack(data)),
+                                Collections.singletonList(enchantedBucket)));
+                    }
+                });
+            }
+            if (!recipes.isEmpty()) {
+                registration.addRecipes(recipes, VanillaRecipeCategoryUid.ANVIL);
+            }
+        }
     }
 
 }
