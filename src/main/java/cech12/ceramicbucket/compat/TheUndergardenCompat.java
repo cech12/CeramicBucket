c47da4446880dfddaa5ab6e308eed56c734dd981
Christian Voß <cech12@gmail.com>
2021-01-13 12:59:07 +0100

    add advancement for undergarden mod #43

--- src/main/java/cech12/ceramicbucket/compat/TheUndergardenCompat.java
+++ src/main/java/cech12/ceramicbucket/compat/TheUndergardenCompat.java
@@ -1,10 +1,12 @@
 package cech12.ceramicbucket.compat;
 
 import cech12.ceramicbucket.api.data.ObtainableEntityType;
+import net.minecraft.entity.Entity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +14,14 @@ public class TheUndergardenCompat extends ModCompat.Mod implements ModCompat.Ent
 
     private final List<ObtainableEntityType> obtainableEntityTypes = new ArrayList<>();
 
-    public TheUndergardenCompat() {
-        super("undergarden");
-        this.addFish("gwibling");
-    }
+    private static final String MOD_NAME = "undergarden";
 
-    private void addFish(String fish) {
-        this.obtainableEntityTypes.add(new ObtainableEntityType.Builder(new ResourceLocation(this.name, fish), Fluids.WATER).addFluidTag(FluidTags.WATER).build());
+    private static final ResourceLocation GWIBLING = new ResourceLocation(MOD_NAME, "gwibling");
+    private static final ResourceLocation CATCH_GWIBLING = new ResourceLocation(MOD_NAME, "catch_gwibling");
+
+    public TheUndergardenCompat() {
+        super(MOD_NAME);
+        this.obtainableEntityTypes.add(new ObtainableEntityType.Builder(GWIBLING, Fluids.WATER).addFluidTag(FluidTags.WATER).build());
     }
 
     @Override
@@ -26,4 +29,11 @@ public class TheUndergardenCompat extends ModCompat.Mod implements ModCompat.Ent
         return this.obtainableEntityTypes;
     }
 
+    @Override
+    public ResourceLocation getEntityObtainingAdvancement(@Nonnull Entity entity) {
+        if (GWIBLING.equals(entity.getType().getRegistryName())) {
+            return CATCH_GWIBLING;
+        }
+        return null;
+    }
 }
